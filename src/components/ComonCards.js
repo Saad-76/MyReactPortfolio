3326eeff9cdf3faea805c6eecfbba737417a085f
Saad Mehmood <saadmehmood131@gmail.com>
2023-12-14 00:10:30 +0500

    resume file update

--- src/components/ComonCards.js
+++ src/components/ComonCards.js
@@ -6,7 +6,7 @@ const CommonCards = () => (
     <div className="card-content">
       <h2 className="card-title">Something Awesome</h2>
       <p className="card-body">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, culpa...
       </p>
       <a href="#" className="button">
         Learn More
